60edb63fb7a2bd068c2307e34abd8fe526015dd6
leongxj123 <113355197+leongxj123@users.noreply.github.com>
2024-11-03 15:26:58 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -230,7 +230,7 @@ export function SideBar(props: { className?: string }) {
       {...props}
     >
       <SideBarHeader
-        title="NextChat"
+        title="2023GPT"
         subTitle="Build your own AI assistant."
         logo={<ChatGptIcon />}
         shouldNarrow={shouldNarrow}
