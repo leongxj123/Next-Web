258f1795fde72ff6606617462976075590082a7f
leongxj123 <113355197+leongxj123@users.noreply.github.com>
2024-11-03 15:22:03 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,10 @@ import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "2023GPT",
+  description: "Your personal Chat Bot.",
   appleWebApp: {
-    title: "NextChat",
+    title: "2023GPT",
     statusBarStyle: "default",
   },
 };
